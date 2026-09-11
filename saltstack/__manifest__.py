b452278e4499c37cc57d16b3d901ce4251c4bbb6
Jakob Krabbe <jakob.krabbe@vertel.se>
2026-09-11 15:23:06 +0200

    chore: bump 18.0.1.35.1

--- saltstack/__manifest__.py
+++ saltstack/__manifest__.py
@@ -2,7 +2,7 @@
 
 {
     'name': 'SaltStack Infrastructure',
-    'version': '18.0.1.35.0',
+    'version': '18.0.1.35.1',
     'category': 'Infrastructure',
     'summary': 'Manage SaltStack minions, pillar anchors and infrastructure',
     'description': """
